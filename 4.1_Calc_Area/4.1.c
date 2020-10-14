618de411562b756e3e02f15b62b243c81556b17f
Antonio <dongyixiao2001@163.com>
2020-10-14 17:11:31 +0800

    Revert "Update ch4"
    This reverts commit e4a76d34fe2152c32c6859c2ba064de1a3aa97a9.

--- 4.1_Calc_Area/4.1.c
+++ 4.1_Calc_Area/4.1.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #include<math.h>
 
-#define ratio (3.14/180)  //角度转弧度
+#define PI 3.14
 
 int main()
 {
     freopen("./Temp/test.in","r",stdin);
-    double a,b,c,d,s,S,degree;
+    double a,b,c,d;
+    double s,S,degree;
     scanf("%lf%lf%lf%lf%lf",&a,&b,&c,&d,&degree);
     degree/=2;
 
     s=(a+b+c+d)/2;
-    S=sqrt((s-a)*(s-b)*(s-c)*(s-d)-a*b*c*d*pow(cos(degree*ratio),2));
-
-
+    S=sqrt((s-a)*(s-b)*(s-c)*(s-d)-a*b*c*d*pow(cos(degree),2));
+    printf("%lf\n",pow(cos(degree),2));
     printf("%lf\n",S);
 
 
