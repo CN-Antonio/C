c03fa0d5d7e432f79b19f5f57b2cf5b087e4be8b
Antonio <dongyixiao2001@163.com>
2020-10-14 17:11:31 +0800

    Revert "Update ch4"
    This reverts commit e4a76d34fe2152c32c6859c2ba064de1a3aa97a9.

--- 4.2_Calc_root/4.2.c
+++ 4.2_Calc_root/4.2.c
@@ -1,27 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 
-double Root1(int A,int B,int C)
-{
-    double root;
-    root = 0.5*(-B+sqrt(pow(B,2)-(4*A*C)));
-    return root;
-}
-
 int main()
 {
     freopen("./Temp/test.in","r",stdin);
     int a,b,c;
     scanf("%d,%d,%d",&a,&b,&c);
-
+    printf("%d %d %d\n",a,b,c);
     double r1,r2;
-    r1 = (-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
-    r2 = (-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);
+    r1 = 0.5*(-b + sqrt(pow(b,2)-4*a*c));
+    r2 = 0.5*(-b - sqrt(pow(b,2)-4*a*c));
     printf("%lf,%lf\n",r1,r2);
-
-
 
     return 0;
 
 }
-
