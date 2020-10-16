4f6c92f35b37a7b32fac8afd4dc9d0504095ab1f
Antonio <dongyixiao2001@163.com>
2020-10-16 08:05:43 +0800

    Finish ch4.

--- 4.2_Calc_root/4.2.c
+++ 4.2_Calc_root/4.2.c
@@ -6,10 +6,10 @@ int main()
     // freopen("./Temp/test.in","r",stdin);
     int a,b,c;
     scanf("%d,%d,%d",&a,&b,&c);
-    printf("%d %d %d\n",a,b,c);
+    // printf("%d %d %d\n",a,b,c);
     double r1,r2;
-    r1 = 0.5*(-b + sqrt(pow(b,2)-4*a*c));
-    r2 = 0.5*(-b - sqrt(pow(b,2)-4*a*c));
+    r1 = 0.5*(-b + sqrt(pow(b,2)-4*a*c))/a;
+    r2 = 0.5*(-b - sqrt(pow(b,2)-4*a*c))/a;
     printf("%lf,%lf\n",r1,r2);
 
     return 0;
